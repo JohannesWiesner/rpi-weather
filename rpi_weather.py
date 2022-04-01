1947a50aeba8f26bdaf7f200ac981f646c4740c6
Johannes Wiesner <joh.wiesner@gmail.com>
2022-04-01 17:09:14 +0200

    changed xrange to range. Updated code to latest version of adafruit library: write_display is replaced with show(), set_pixel() is replaced ẃith pixel(), begin() method has not to be called anymore.

--- rpi_weather.py
+++ rpi_weather.py
@@ -8,7 +8,7 @@
 #===============================================================================
 from time import sleep
 
-from Adafruit_LED_Backpack import Matrix8x8
+from adafruit_ht16k33 import matrix
 from led8x8icons import LED8x8ICONS
 
 class RpiWeather():
@@ -16,68 +16,66 @@ class RpiWeather():
     
     def __init__(self, ):
         self.matrix = []
-        self.matrix.append(Matrix8x8.Matrix8x8(address=0x70, busnum=1))
-        self.matrix.append(Matrix8x8.Matrix8x8(address=0x71, busnum=1))
-        self.matrix.append(Matrix8x8.Matrix8x8(address=0x72, busnum=1))
-        self.matrix.append(Matrix8x8.Matrix8x8(address=0x73, busnum=1))
-        for m in self.matrix:
-            m.begin()
-          
+        self.matrix.append(matrix.Matrix8x8(address=0x70, busnum=1))
+        self.matrix.append(matrix.Matrix8x8(address=0x71, busnum=1))
+        self.matrix.append(matrix.Matrix8x8(address=0x72, busnum=1))
+        self.matrix.append(matrix.Matrix8x8(address=0x73, busnum=1))
+
     def is_valid_matrix(self, matrix):
         """Returns True if matrix number is valid, otherwise False."""
-        return matrix in xrange(len(self.matrix))     
+        return matrix in range(len(self.matrix))     
           
     def clear_disp(self, matrix=None):
         """Clear specified matrix. If none specified, clear all."""
         if matrix == None:
             for m in self.matrix:
                 m.clear()
-                m.write_display()
+                m.show()
         else:
             if not self.is_valid_matrix(matrix):
                 return
             self.matrix[matrix].clear()
-            self.matrix[matrix].write_display()
+            self.matrix[matrix].show()
             
     def set_pixel(self, x, y, matrix=0, value=1):
         """Set pixel at position x, y for specified matrix to the given value."""
         if not self.is_valid_matrix(matrix):
             return
-        self.matrix[matrix].set_pixel(x, y, value)
-        self.matrix[matrix].write_display()
+        self.matrix[matrix].pixel(x, y, value)
+        self.matrix[matrix].show()
           
     def set_bitmap(self, bitmap, matrix=0):
         """Set specified matrix to provided bitmap."""
         if not self.is_valid_matrix(matrix):
             return
-        for x in xrange(8):
-            for y in xrange(8):
-                self.matrix[matrix].set_pixel(x, y, bitmap[y][x])
-        self.matrix[matrix].write_display()
+        for x in range(8):
+            for y in range(8):
+                self.matrix[matrix].pixel(x, y, bitmap[y][x])
+        self.matrix[matrix].show()
         
     def set_raw64(self, value, matrix=0):
         """Set specified matrix to bitmap defined by 64 bit value."""
         if not self.is_valid_matrix(matrix):
             return        
         self.matrix[matrix].clear()
-        for y in xrange(8):
+        for y in range(8):
             row_byte = value >> (8*y)
-            for x in xrange(8):
+            for x in range(8):
                 pixel_bit = row_byte >> x & 0x01 
-                self.matrix[matrix].set_pixel(x, y, pixel_bit) 
-        self.matrix[matrix].write_display()
+                self.matrix[matrix].pixel(x, y, pixel_bit) 
+        self.matrix[matrix].show()
         
     def scroll_raw64(self, value, matrix=0, delay=0.15):
         """Scroll out the current bitmap with the supplied bitmap. Can also
         specify a matrix (0-3) and a delay to set scroll rate.
         """
-        for step in xrange(7,-1,-1):
-            for old_row in xrange(7,0,-1):
+        for step in range(7,-1,-1):
+            for old_row in range(7,0,-1):
                 self.matrix[matrix].buffer[old_row*2] = self.matrix[matrix].buffer[(old_row-1)*2]
             new_row = (value >> (8*step)) & 0xff
             new_row = (new_row << 7 | new_row >> 1) & 0xff  #fix for memory buffer error
             self.matrix[matrix].buffer[0] = new_row
-            self.matrix[matrix].write_display()
+            self.matrix[matrix].show()
             sleep(delay)   
         
     def disp_number(self, number):
